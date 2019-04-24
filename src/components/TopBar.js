b97c936d04a4d9a3f1f2c946d0a897ff86b655b0
Andrey Zhuravlyov <crane.andrey@gmail.com>
2019-04-24 22:01:30 +0300

    buttons in top bar

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -4,21 +4,49 @@ import React from 'react';
 import type { Node } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
 
-import LockOpen from '@material-ui/icons/LockOpen';
 
 export function TopBarComponent(): Node {
   return (
     <AppBar position="relative">
       <Toolbar>
-        <Typography variant="h6" color="inherit">
-          Protected Text
-        </Typography>
-        <IconButton>
-          <LockOpen />
-        </IconButton>
+        <Grid
+          container
+          direction="row"
+          justify="flex-start"
+          alignItems="center"
+        >
+          <Grid item xs={1}>
+            <Typography variant="h6" color="inherit">
+              Protected Text
+            </Typography>
+          </Grid>
+          <Grid item xs={11}>
+            <Grid
+              container
+              direction="row"
+              justify="flex-end"
+              alignItems="center"
+              spacing={8}
+            >
+              <Grid item>
+                <Button variant="contained">Reload</Button>
+              </Grid>
+              <Grid item>
+                <Button variant="contained">Save</Button>
+              </Grid>
+              <Grid item>
+                <Button variant="contained">Change password</Button>
+              </Grid>
+              <Grid item>
+                <Button variant="contained">Delete</Button>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
       </Toolbar>
     </AppBar>
   );
