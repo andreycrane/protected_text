1073c79221ea655052e5a9240362f81c45c7999a
Andrey Zhuravlyov <crane.andrey@gmail.com>
2019-05-02 15:08:19 +0300

    server application druft

--- src/server/application.js
+++ src/server/application.js
@@ -1,11 +1,43 @@
 'use strict';
 
 const express = require('express');
+const Keyv = require('keyv');
+const bodyParser = require('body-parser');
 
-const app = express();
+module.exports = config => {
+  const app = express();
+  const db = new Keyv(...config.connectionParams);
 
-app.get('/id/:id', async (req, res) => {});
-app.post('/id/:id', async (req, res) => {});
-app.delete('/id/:id', async (req, res) => {});
+  app.use(bodyParser.json());
 
-module.exports = app;
+  app.get('/id/:id', async (req, res) => {
+    const { id } = req.params;
+    const data = await db.get(id);
+
+    if (!data) {
+      res.json({ id, data: null });
+      return;
+    }
+
+    res.json({ id, data });
+  });
+
+  app.post('/id/:id', async (req, res) => {
+    const { id } = req.params;
+    const { body } = req;
+
+    await db.set(id, body);
+
+    res.json({ id, data: body });
+  });
+
+  app.delete('/id/:id', async (req, res) => {
+    const { id } = req.params;
+
+    await db.delete(id);
+
+    res.json({ id, data: null });
+  });
+
+  return { app, db };
+};
--- src/server/application.test.js
+++ src/server/application.test.js
@@ -1,3 +1,126 @@
-describe('server#application', () => {
-  it('ok', () => {});
+import request from 'supertest';
+import { random } from 'faker';
+import createApp from './application';
+
+describe('server#application', function () {
+  beforeAll(() => {
+    this.namespace = random.uuid();
+    this.store = new Map();
+    const { app, db } = createApp({
+      connectionParams: [
+        {
+          namespace: this.namespace,
+          store: this.store,
+        },
+      ],
+    });
+
+    this.app = app;
+    this.db = db;
+  });
+
+  beforeEach(async () => {
+    await this.db.clear();
+  });
+
+  it('returns data=null if key doesn\'t exist', (done) => {
+    const id = random.uuid();
+    request(this.app)
+      .get(`/id/${id}`)
+      .expect('Content-Type', /json/)
+      .expect(
+        200,
+        { id, data: null },
+        done,
+      );
+  });
+
+  it('returns data=\'data\' if key exists', async () => {
+    const id = random.uuid();
+    const data = {
+      [random.objectElement()]: random.uuid(),
+    };
+
+    this.db.set(id, data);
+
+    return request(this.app)
+      .get(`/id/${id}`)
+      .expect('Content-Type', /json/)
+      .expect(
+        200,
+        { id, data },
+      );
+  });
+
+  it('stores data by certain key', async () => {
+    const id = random.uuid();
+    const data = {
+      [random.objectElement()]: random.uuid(),
+    };
+
+    return request(this.app)
+      .post(`/id/${id}`)
+      .send(data)
+      .expect('Content-Type', /json/)
+      .expect(
+        200,
+        { id, data },
+      );
+  });
+
+  it('returns recently saved data', async () => {
+    const id = random.uuid();
+    const data = {
+      [random.objectElement()]: random.uuid(),
+    };
+
+    await request(this.app)
+      .post(`/id/${id}`)
+      .send(data)
+      .expect('Content-Type', /json/)
+      .expect(
+        200,
+        { id, data },
+      );
+
+    return request(this.app)
+      .get(`/id/${id}`)
+      .expect('Content-Type', /json/)
+      .expect(
+        200,
+        { id, data },
+      );
+  });
+
+  it('removes recently stored data', async () => {
+    const id = random.uuid();
+    const data = {
+      [random.objectElement()]: random.uuid(),
+    };
+
+    await request(this.app)
+      .post(`/id/${id}`)
+      .send(data)
+      .expect('Content-Type', /json/)
+      .expect(
+        200,
+        { id, data },
+      );
+
+    await request(this.app)
+      .get(`/id/${id}`)
+      .expect('Content-Type', /json/)
+      .expect(
+        200,
+        { id, data },
+      );
+
+    await request(this.app)
+      .delete(`/id/${id}`)
+      .expect('Content-Type', /json/)
+      .expect(
+        200,
+        { id, data: null },
+      );
+  });
 });
