c2ebe5c45f88a1f3e76798fa81b2f29eae3ba47c
Andrey Zhuravlyov <crane.andrey@gmail.com>
2019-05-04 20:13:49 +0300

    main page druft

--- src/client/components/MainPage/index.js
+++ src/client/components/MainPage/index.js
@@ -3,8 +3,87 @@
 import React from 'react';
 import type { Node } from 'react';
 
-export default function MainPage(): Node {
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = (): TStyles => ({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'stretch',
+
+    width: '100vw',
+    height: '100vh',
+  },
+  main: {
+    flexGrow: '1',
+
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  paper: {
+    minWidth: '400px',
+    margin: '120px auto',
+    padding: '10px 15px',
+  },
+});
+
+export type TProps = $ReadOnly<{
+  classes: TStyles,
+}>;
+
+
+export function MainPageComponent(props: TProps): Node {
+  const { classes } = props;
+
   return (
-    <h1>Hello world!</h1>
+    <div className={classes.container}>
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" color="inherit" noWrap>
+            Protected Text
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <div className={classes.main}>
+        <Paper
+          square
+          className={classes.paper}
+        >
+          <Typography variant="h6">
+            {'Go to protectedText.com/'}
+          </Typography>
+          <form noValidate autoComplete="off">
+            <TextField
+              id="site-name"
+              label="Site name"
+              margin="normal"
+              fullWidth
+            />
+            <Button
+              type="button"
+              fullWidth
+              variant="contained"
+              size="large"
+              color="primary"
+            >
+              Go
+            </Button>
+          </form>
+        </Paper>
+      </div>
+    </div>
   );
 }
+
+export default withStyles(styles)(MainPageComponent);
