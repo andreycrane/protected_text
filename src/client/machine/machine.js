f8ceee605a31570e23388102fb94fae0bb5c01d2
Andrey Zhuravlyov <crane.andrey@gmail.com>
2019-05-05 20:54:29 +0300

    new deleting state

--- src/client/machine/machine.js
+++ src/client/machine/machine.js
@@ -267,6 +267,31 @@ const machine = Machine(
           },
         },
       },
+      DELETING: {
+        initial: 'confirm',
+        states: {
+          confirm: {
+            on: {
+              OK: 'deleting',
+              CANCEL: '#machine.MODIFIED',
+            },
+          },
+          deleting: {
+            invoke: {
+              id: 'deleting',
+              src: 'deleteSite',
+              onDone: '#machine.EXIT',
+              onError: 'error',
+            },
+          },
+          error: {
+            on: {
+              REPEAT: 'deleting',
+              CANCEL: '#machine.MODIFIED',
+            },
+          },
+        },
+      },
       EXIT: {
         type: 'final',
       },
--- src/client/machine/machine.test.js
+++ src/client/machine/machine.test.js
@@ -8,12 +8,14 @@ import initContext from './context';
 import {
   getSiteService,
   postSiteService,
+  deleteSiteService,
 } from './services';
 
 jest.mock('./services', () => {
   const t = jest.requireActual('./services');
   t.getSiteService = jest.fn(async () => null);
   t.postSiteService = jest.fn(async () => null);
+  t.deleteSiteService = jest.fn(async () => null);
 
   return t;
 });
@@ -884,6 +886,141 @@ describe('machine', () => {
           }
         })
         .start(SavingState)
+        .send('CANCEL');
+    });
+  });
+
+  describe('DELETING state', () => {
+    it('moves to DELETING:confirm children state', (done) => {
+      const context = initContext('site_name');
+      const testMachine = machine.withContext(context);
+      const DeletingState = State.create({
+        value: 'DELETING',
+        context,
+      });
+
+      interpret(testMachine)
+        .onTransition((state) => {
+          if (state.matches({ DELETING: 'confirm' })) {
+            done();
+          }
+        })
+        .start(DeletingState);
+    });
+
+    it('moves from DELETING:confirm to MODIFIED on CANCEL', (done) => {
+      const context = initContext('site_name');
+      const testMachine = machine.withContext(context);
+      const DeletingState = State.create({
+        value: { DELETING: 'confirm' },
+        context,
+      });
+
+      interpret(testMachine)
+        .onTransition((state) => {
+          if (state.matches('MODIFIED')) {
+            done();
+          }
+        })
+        .start(DeletingState)
+        .send('CANCEL');
+    });
+
+    it('moves from DELETING:confirm to DELETING:deleting on OK', (done) => {
+      const context = initContext('site_name');
+      const testMachine = machine.withContext(context);
+      const DeletingState = State.create({
+        value: { DELETING: 'confirm' },
+        context,
+      });
+
+      interpret(testMachine)
+        .onTransition((state) => {
+          if (state.matches({ DELETING: 'deleting' })) {
+            done();
+          }
+        })
+        .start(DeletingState)
+        .send('OK');
+    });
+
+    it('moves from DELETING:deleting to EXIT if service resolves', (done) => {
+      const context = initContext('site_name');
+      const testMachine = machine.withContext(context);
+
+      const DeletingState = State.create({
+        value: 'DELETING',
+        context,
+      });
+
+      deleteSiteService.mockImplementation(async () => true);
+
+      interpret(testMachine)
+        .onTransition((state) => {
+          if (state.matches('EXIT')) {
+            done();
+          }
+        })
+        .start(DeletingState)
+        .send('OK');
+    });
+
+    it('moves from DELETING:deleting to DELETING:error if service rejects', (done) => {
+      const context = initContext('site_name');
+      const testMachine = machine.withContext(context);
+
+      const DeletingState = State.create({
+        value: 'DELETING',
+        context,
+      });
+
+      deleteSiteService.mockImplementation(async () => { throw new Error(); });
+
+      interpret(testMachine)
+        .onTransition((state) => {
+          if (state.matches({ DELETING: 'error' })) {
+            done();
+          }
+        })
+        .start(DeletingState)
+        .send('OK');
+    });
+
+    it('moves from DELETING:error to DELETING:deleting on REPEAT', (done) => {
+      const context = initContext('site_name');
+      const testMachine = machine.withContext(context);
+
+      const DeletingState = State.create({
+        value: { DELETING: 'error' },
+        context,
+      });
+
+      interpret(testMachine)
+        .onTransition((state) => {
+          if (state.matches({ DELETING: 'deleting' })) {
+            done();
+          }
+        })
+        .start(DeletingState)
+        .send('REPEAT');
+    });
+
+    it('moves from DELETING:error to MODIFIED on CANCEL', (done) => {
+      const context = initContext('site_name');
+      const testMachine = machine.withContext(context);
+
+      const DeletingState = State.create({
+        value: { DELETING: 'error' },
+        context,
+      });
+
+      interpret(testMachine)
+        .onTransition((state) => {
+          if (state.matches('MODIFIED')) {
+            done();
+          }
+        })
+        .start(DeletingState)
         .send('CANCEL');
     });
   });
