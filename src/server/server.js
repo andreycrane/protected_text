49e6f6c1ff1ff85dd40d4111d96592f4279a5b27
Andrey Zhuravlyov <crane.andrey@gmail.com>
2019-05-09 14:00:20 +0300

    FIX: missing prodApp params

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,6 @@
 const { prodApp } = require('./application');
 
 const port = process.env.PORT || 5000;
-const { app } = prodApp();
+const { app } = prodApp({});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
