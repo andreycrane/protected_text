abf2e5862c99ac9f42030de7a0fd31333743d422
Andrey Zhuravlyov <crane.andrey@gmail.com>
2019-05-25 13:22:42 +0300

    a bit of bundle optimization

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,6 +1,52 @@
+const path = require('path');
 const baseConfig = require('./webpack.base.config');
+//const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   ...baseConfig,
   mode: 'production',
+  entry: {
+    index: './src/client/index.js',
+    main_page: './src/client/components/MainPage',
+    notes_page: './src/client/components/NotesPage',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  optimization: {
+    splitChunks: {
+      maxInitialRequests: 10,
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          chunks: 'all',
+          name: 'commons',
+        },
+        draft: {
+          test: /[\\/]node_modules[\\/]draft-js/,
+          priority: 10,
+          chunks: 'all',
+          name: 'draft',
+        },
+        react: {
+          test: /[\\/]node_modules[\\/](react|react-dom|react-router)/,
+          priority: 10,
+          chunks: 'all',
+          name: 'react',
+        },
+        material: {
+          test: /[\\/]node_modules[\\/]@material-ui/,
+          priority: 10,
+          chunks: 'all',
+          name: 'material',
+        },
+      },
+    },
+  },
+  plugins: [
+    ...baseConfig.plugins,
+    //new BundleAnalyzerPlugin(),
+  ],
 };
